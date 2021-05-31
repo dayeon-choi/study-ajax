5db078add625e3a16ee288cc4706b392e6106849
dayeon-choi <dayeon2399@naver.com>
2021-05-31 15:32:24 +0900

    Update ex3-json03 api 주제 정함

---
+++ c6_ex3(mashup)/ex03_json_ex/childSchoolSafety.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+  var interval = setInterval(handleRefresh, 3000);
+  handleRefresh();
+};
+function handleRefresh() {
+  console.log("here");
+  var url =
+    "http://openapi.seoul.go.kr:8088/486e485945646179313036596c477073/xml/childSchoolSafetyEdu_jn/1/200/";
+  $.getJSON(url, updateChildSchoolSafety);
+}
+
+function updateChildSchoolSafety(lectures) {
+  var lecturesDiv = document.getElementById("childSchoolSafety");
+  lectures = lectures.GwanakClassLectureList.row;
+  for (var i = 0; i < lectures.length; i++) {
+    var lecture = lectures[i];
+    var div = document.createElement("div");
+    div.setAttribute("class", "childSchoolSafety");
+    div.innerHTML =
+      "강좌명:" +
+      lecture.TITLE +
+      "은 " +
+      " 교육기간:" +
+      lecture.EDU_PERIOD +
+      " 교육장소:" +
+      lecture.EDU_PLACE +
+      " 교육대상:" +
+      lecture.EDU_TARGET +
+      " 접수방법:" +
+      lecture.APPLY_WAY +
+      " 수걍료:" +
+      lecture.EDU_PAY +
+      "원입니다.";
+
+    lecturesDiv.appendChild(div);
+  }
+}
